88ce16769441e35b201334249b869caafe7d16e9
Matteo Fiordarancio <matteofiordarancio@gmail.com>
2020-04-16 14:57:28 +0200

    Designed the login page

--- app/src/main/java/com/uncookthebook/app/LoginFragment.java
+++ app/src/main/java/com/uncookthebook/app/LoginFragment.java
@@ -24,30 +24,30 @@ public class LoginFragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.login_fragment, container, false);
-        final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
-        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
-        MaterialButton nextButton = view.findViewById(R.id.next_button);
+//        final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
+//        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+        MaterialButton nextButton = view.findViewById(R.id.login_button);
 
         // Set an error if the password is less than 8 characters.
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!isPasswordValid(passwordEditText.getText())) {
-                    passwordTextInput.setError(getString(R.string.error_password));
-                } else {
-                    passwordTextInput.setError(null); // Clear the error
+//                if (!isPasswordValid(passwordEditText.getText())) {
+//                    passwordTextInput.setError(getString(R.string.error_password));
+//                } else {
+//                    passwordTextInput.setError(null); // Clear the error
                     ((NavigationHost) getActivity()).navigateTo(new ReportArticleFragment(), false);
-                }
+//                }
             }
         });
 
         // Clear the error once more than 8 characters are typed.
-        passwordEditText.setOnKeyListener((view1, i, keyEvent) -> {
-            if (isPasswordValid(passwordEditText.getText())) {
-                passwordTextInput.setError(null); //Clear the error
-            }
-            return false;
-        });
+//        passwordEditText.setOnKeyListener((view1, i, keyEvent) -> {
+//            if (isPasswordValid(passwordEditText.getText())) {
+//                passwordTextInput.setError(null); //Clear the error
+//            }
+//            return false;
+//        });
 
         return view;
     }
